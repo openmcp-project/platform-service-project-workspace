b984d52f4f0f6842d93129b48775e919bed61521
René Schünemann (SAP) <rene.schuenemann@sap.com>
2026-08-28 16:25:43 +0200

    chore: enable FIPS 140-3 verification
    On-behalf-of: @SAP rene.schuenemann@sap.com
    Signed-off-by: René Schünemann (SAP) <rene.schuenemann@sap.com>

--- cmd/platform-service-project-workspace/main.go
+++ cmd/platform-service-project-workspace/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/openmcp-project/controller-utils/pkg/fips"
 	"github.com/openmcp-project/platform-service-project-workspace/cmd/platform-service-project-workspace/app"
 )
 
 func main() {
 	cmd := app.NewPlatformServiceProjectWorkspaceCommand()
+
+	fips.Verify(context.Background())
 
 	if err := cmd.Execute(); err != nil {
 		fmt.Print(err)
